78c8fb49470c451f0fc43267352b26b925149089
geo-silva <rumblycactus3@gmail.com>
2022-06-10 10:59:54 -0300

    Primeiro Commit com classe main

---
+++ Academia 2022/src/projeto/academia/ano2022/Main.java
@@ -0,0 +1,13 @@
+package projeto.academia.ano2022;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Olá!");
+
+	}
+
+}
